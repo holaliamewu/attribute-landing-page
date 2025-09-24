eafe4dd2e8a0c811551df4dfc86c3a3a634c2cdc
Amewu Emmanuel Mensah <me.amewu@gmail.com>
2025-09-24 00:35:27 +0000

    fix: width issue

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Hero from "./components/hero";
 
 export default function Home( ) {
   return(
-  <div className="bg-white">
+  <div className="min-w-screen bg-white">
 		<Header />
 		<Hero />
 		<Footer />
